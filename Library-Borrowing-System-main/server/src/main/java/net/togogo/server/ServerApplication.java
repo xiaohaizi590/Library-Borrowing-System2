a2fdc57e575e5d3cb04f769bdc67f24a8a1797e2
xiaohaizi590 <1485890233@qq.com>
2026-07-24 11:06:28 +0800

    注释Nacos相关配置和依赖，移除微服务注册中心

--- Library-Borrowing-System-main/server/src/main/java/net/togogo/server/ServerApplication.java
+++ Library-Borrowing-System-main/server/src/main/java/net/togogo/server/ServerApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "net.togogo")
 @EnableJpaRepositories(basePackages = "net.togogo.repository")
 @EntityScan(basePackages = "net.togogo.entity")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableCaching
 public class ServerApplication {
 
